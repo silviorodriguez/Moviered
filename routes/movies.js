a225e201d15f6e9493f95214a4b8d845c60a5216
silviorodriguez <118927826+silviorodriguez@users.noreply.github.com>
2025-02-26 07:31:28 -0500

    Update movies.js
    endpoint movie por id

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { movieGet, moviePost, moviePut, movieDel } = require('../controllers/movies')
+const { movieGet, moviePost, moviePut, movieDel, movieGetById } = require('../controllers/movies')
 const { protect } = require('../middlewares/authMiddleware')
 
 const router = Router()
@@ -8,5 +8,7 @@ router.get('/', movieGet)
 router.post('/', protect, moviePost)
 router.put('/:id', protect, moviePut)
 router.delete('/:id', protect, movieDel)
+//endpoint de movies por id
+router.get('/:id', movieGetById)
 
 module.exports = router
